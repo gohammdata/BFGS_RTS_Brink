d7fb88f4343ac096f17d437e99c8d039154ded74
hammdata <gohammdata@gmail.com>
2020-06-16 09:05:12 -0400

    Pom configured, successful Maven Install v1, Test added, package structure created

---
+++ src/main/java/com/BFGS/main/Main.java
@@ -0,0 +1,7 @@
+package com.BFGS.main;
+
+public class Main {
+    public String welcomeToBrink() {
+        return "Welcome to Brink, Young Strategist";
+    }
+}
---
+++ src/test/java/com/BFGS/main/MainTest.java
@@ -0,0 +1,5 @@
+package com.BFGS.main;
+
+public class MainTest {
+
+}
